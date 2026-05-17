37f072c5f10d8c240650637c61d7e3339b6dbe01
Obot83 <35242414+Obot83@users.noreply.github.com>
2026-05-17 17:36:19 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1021,7 +1021,7 @@ export default function App() {
         <div className="flex gap-3 md:gap-4 w-full md:w-auto justify-center">
           <div className="hidden lg:flex items-center gap-2 px-3 md:px-4 py-2 bg-slate-50 dark:bg-slate-800 border border-slate-200 dark:border-slate-700 rounded-lg text-[9px] md:text-xs font-semibold uppercase tracking-wider text-slate-600 dark:text-slate-400 whitespace-nowrap">
             <span className="w-2 h-2 bg-green-500 rounded-full animate-pulse shrink-0"></span>
-            Available For New Projects
+            
           </div>
           <button 
             onClick={() => {
